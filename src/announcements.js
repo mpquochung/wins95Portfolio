a4ca693ac337dded919ed7ac91ddb099acce6d4b
mpquochung <maphquochung@gmail.com>
2026-03-26 13:27:17 +0700

    news

--- src/announcements.js
+++ src/announcements.js
@@ -1,6 +1,16 @@
 // Add your announcements here (newest first).
 export default [
   {
+    title: 'Paper Accepted',
+    date: '04-2026',
+    text: '2 papers are accepted to ACL 2026.',
+  },
+  {
+    title: 'Professional Service',
+    date: '03-2026',
+    text: 'I am serving as a reviewer at UAI 2026.',
+  },
+  {
     title: 'New role',
     date: '05-10-2025',
     text: 'I am joining N2TP Solution JSC as an AI Scientist.',
